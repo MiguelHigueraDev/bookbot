b5bb4e88b8c455a48307a1793d63579ad5cca36c
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-06-29 13:30:55 -0400

    print book stats

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from stats import count_words
+from stats import count_characters
+import sys
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
         
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1]
+    text = get_book_text(path)
     num_words = count_words(text)
-    print(f"{num_words} words found in the document")
+    character_counts = count_characters(text)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in character_counts:
+        if char.isalpha():
+            count = character_counts[char]
+            print(f"{char}: {count}")
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    counts = {}
+    lowercase_text = text.lower()
+    for word in lowercase_text:
+        for letter in word:
+            if letter in counts:
+                counts[letter] += 1
+            else:
+                counts[letter] = 1
+                
+    return counts
+
+def sort_char_counts(character_dict):
+    return dict(sorted(character_dict.items(), key=lambda item: item[1], reverse=True))
